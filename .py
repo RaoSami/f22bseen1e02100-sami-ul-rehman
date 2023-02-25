ad000df4095652307196652d8bdcf5d4d4c12dde
Rao Sami <126256738+RaoSami@users.noreply.github.com>
2023-02-25 01:10:05 -0800

    project oop
    academic session

---
+++ .py
@@ -0,0 +1,17 @@
+class_init_(self,semester,program offered,faculty departments,course offered):
+self_semester=semeste
+self_program offered=program offered
+self_faculty department=faculty department
+self_course offfered=course offered
+seif_campus=campus
+def_time duration_six month_(self):
+print(f,{self_semester}, "time duration six month",)
+class_program offered(program):
+def_BS Sofware Engineering_program offered(self):
+print(f,{self_program offered},"BS Sofware Engineering",)
+class_faculty department(departments):
+def_old faculty Engineering_faculty department(self):
+class_course offered(Acadenic Session)
+def_name of subjects_course offered(self):
+print(f,{self_course offered},"name of subjects",)
+
