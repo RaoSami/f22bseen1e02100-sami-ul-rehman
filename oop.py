9c8520a631697de38d0834a831b787cc122b0cc2
Rao Sami <126256738+RaoSami@users.noreply.github.com>
2023-03-05 20:36:46 -0800

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,12 @@
+class courses():
+    def __init__(self,program):
+        pass
+    program="software engeenring"
+    def data_science(self):
+        print( f"This is my course {self.program}")
+a=courses("bsse")
+a.name="sami ul rehman"
+
+
+a.data_science()
+
